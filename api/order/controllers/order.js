84fcd968817f1dc8cdd4bb57444a7de3c3e458af
kukajp3 <kukajp@gmail.com>
2021-05-26 23:25:25 -0300

    .

---
+++ api/order/controllers/order.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const stripe = require('stripe')(process.env.STRIPE_KEY)
+const {sanitizeEntity} = require('strapi-utils')
+const orderTemplate = require("../../../config/email-templates/order");
+
+module.exports = {
+  createPaymentIntent: async (ctx) => {
+    const { cart } = ctx.request.body;
+
+    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart);
+
+    const games = await strapi.config.functions.cart.cartItems(cartGamesIds);
+
+    if(!games.length){
+      ctx.response.status = 404;
+      return {
+        error: "No valid games found!",
+      };
+    }
+
+    const total = await strapi.config.functions.cart.total(games);
+
+    if(total === 0){
+      return {
+        freeGames: true,
+      };
+    }
+
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: total,
+        currency: "usd",
+        metadata:{ cart: JSON.stringify(cartGamesIds)}
+      });
+
+      return paymentIntent;
+    }catch(err){
+      return {
+        error: err.raw.message,
+      };
+    }
+  },
+
+  create: async(ctx) => {
+    const {cart, paymentIntentId, paymentMethod} = ctx.request.body
+
+    const token = await strapi.plugins["users-permissions"]
+      .services.jwt.getToken(ctx);
+
+    const userId = token.id
+
+    const userInfo = await strapi.query("user", "users-permissions")
+      .findOne({ id: userId });
+
+    const cartGamesIds = await strapi.config.functions.cart.cartGamesIds(cart);
+
+    const games = await strapi.config.functions.cart.cartItems(cartGamesIds);
+
+    const total_in_cents = strapi.config.functions.cart.total(games);
+
+    let paymentInfo;
+    if(total_in_cents !== 0){
+      try{
+        paymentInfo = await stripe.paymentMethods.retrieve(paymentMethod)
+      }catch(err){
+        ctx.response.status = 402;
+        return {error: err.message}
+      }
+    }
+
+    const entry = {
+      total_in_cents,
+      payment_intent_id: paymentIntentId,
+      cart_brand: paymentInfo.card.brand,
+      cart_last4: paymentInfo.card.last4,
+      user: userInfo,
+      games
+    };
+
+    const entity = await strapi.services.order.create(entry);
+
+    await strapi.plugins["email-designer"].services.email.sendTemplatedEmail(
+      {
+        to: userInfo.email,
+        from: "no-reply@wongames.com",
+      },
+      {
+        templateId: 1,
+      },
+      {
+        user: userInfo,
+        payment: {
+          total: `$ ${total_in_cents / 100}`,
+          card_brand: entry.card_brand,
+          card_last4: entry.card_last4,
+        },
+        games,
+      }
+    );
+
+    return sanitizeEntity(entity, {model: strapi.models.order})
+  }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7851dae2-a89d-4da3-a550-2e31e8143b51'
+};
