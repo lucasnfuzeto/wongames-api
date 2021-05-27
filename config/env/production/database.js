414370c1894b619b46fe8a9bf8b051fd8db10361
kukajp3 <kukajp@gmail.com>
2021-05-27 00:58:19 -0300

    .

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -10,7 +10,9 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD'),
-        ssl: true,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {}
     },
